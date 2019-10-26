25a7a3e2979f655517ef6417814cf0c20fef0e96
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-10-26 14:25:54 -0300

    Refactor to clean up unused imports

--- src/main/java/gostack/desafio01/resources/ProjectResource.java
+++ src/main/java/gostack/desafio01/resources/ProjectResource.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.databind.node.TextNode;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
