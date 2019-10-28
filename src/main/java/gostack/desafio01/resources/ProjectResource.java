5060e7af94b14d34ec353264857e6ec7cc4bca10
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-10-28 13:36:47 -0300

    Refactor to use ProjectRepository

--- src/main/java/gostack/desafio01/resources/ProjectResource.java
+++ src/main/java/gostack/desafio01/resources/ProjectResource.java
@@ -4,56 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gostack.desafio01.domain.Project;
+import gostack.desafio01.repository.ProjectRepository;
 
 /**
  * ProjectResource
  */
 @RestController
-@RequestMapping("/projects")
+// @RequestMapping("/projects")
 public class ProjectResource {
     private List<Project> tempProjectStore = new ArrayList<>();
 
-    @GetMapping()
+    @Autowired
+    private ProjectRepository projectRepo;
+
+    @GetMapping("/projects")
     public List<Project> show() {
-
-        return tempProjectStore;
+        return projectRepo.show();
     }
 
-    @PostMapping()
+    @PostMapping("/projects")
     public Project store(@RequestBody Project project) {
-        tempProjectStore.add(project);
-        return project;
+        return projectRepo.store(project);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/projects/{id}")
     public Project update(@RequestBody Project project, @PathVariable Integer id) {
-        Project projectFound = tempProjectStore.stream().filter(p -> p.getId() == id).findFirst().orElse(null);
-        projectFound.setTitle(project.getTitle());
-        return project;
+        return projectRepo.update(project, id);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/projects/{id}")
     public void delete(@PathVariable Integer id) {
-        tempProjectStore.removeIf(p -> p.getId() == id);
+        projectRepo.delete(id);
     }
 
-    @PostMapping("/{id}/tasks")
+    @PostMapping("/projects/{id}/tasks")
     public Project storeTask(@RequestBody Map<String, String> body, @PathVariable Integer id) {
-        Project projectFound = tempProjectStore.stream().filter(p -> p.getId() == id).findFirst().orElse(null);
         // how to get key, value from body whitout object mapping
         // https://medium.com/better-programming/building-a-spring-boot-rest-api-part-ii-7ff1e4384b0b
-        projectFound.setTasks(body.get("title"));
-        return projectFound;
+        // projectFound.setTasks(body.get("title"));
+        return projectRepo.storeTask(body.get("title"), id);
     }
 
 }
